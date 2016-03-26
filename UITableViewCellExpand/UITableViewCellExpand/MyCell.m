16450dba603a0c09d5e4bc2ff9e8ac8da47560d8
Totem <majianglin@totemtec.com>
2016-03-26 22:49:20 +0800

    UITableViewCellExpand use heightConstraint.priority

--- UITableViewCellExpand/UITableViewCellExpand/MyCell.m
+++ UITableViewCellExpand/UITableViewCellExpand/MyCell.m
@@ -14,19 +14,18 @@
 
 @end
 
-const float lowPriorityHeight = 250;
-const float highPriorityHeight = 999;
+
 
 @implementation MyCell
 
 - (void)setContent:(MyContent *)content
 {
     //方式一，改变UILabel行数来限制
-    self.contentLabel.numberOfLines = (content.expand) ? 0 : 3;
+//    self.contentLabel.numberOfLines = (content.expand) ? 0 : 3;
 
     //方式二，设置UILabel的高度约束，比如最高是68，就是3行文字，通过改变约束的优先级来改变高度
     //缺点：最高高度与3行文字高度不是正好匹配，展开或者收缩时，文字稍微有点跳动
-    //self.contentLabelHeightConstraint.priority = (content.expand) ? lowPriorityHeight : highPriorityHeight;
+    self.contentLabelHeightConstraint.priority = (content.expand) ? UILayoutPriorityDefaultLow : UILayoutPriorityRequired-1;
 
     self.contentLabel.text = content.text;
 }
