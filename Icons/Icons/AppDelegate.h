4b53698f2936d041513ae49a51c7b1a2820a950a
Totem <majianglin@totemtec.com>
2016-05-24 23:28:39 +0800

    iOS App Icons Demo

---
+++ Icons/Icons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Icons
+//
+//  Created by majianglin on 5/24/16.
+//  Copyright © 2016 TOTEM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Icons/Icons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Icons
+//
+//  Created by majianglin on 5/24/16.
+//  Copyright © 2016 TOTEM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
