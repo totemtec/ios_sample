87d4034f867edf1dc2db7f98efcef96b5484e0c1
Ma <majianglin@totemtec.com>
2018-08-11 20:16:06 +0800

    Sticky header in UITableView

---
+++ Medium-UITableViewHeader/Medium-UITableViewHeader/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Medium-UITableViewHeader
+//
+//  Created by Yevhenii Veretennikov on 8/27/17.
+//  Copyright © 2017 Yevhenii Veretennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var containerScrollView: UIScrollView!
+    @IBOutlet weak var rootContainerView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var pageControllerContainerView: UIView!
+    
+    @IBOutlet weak var pageControllerContainerTopAnchor: NSLayoutConstraint!
+    @IBOutlet weak var rootContainerHeightAnchor: NSLayoutConstraint!
+    
+    var pageContainerInitialHeight = CGFloat()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        containerScrollView.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // add a top edge inset for table view.
+        tableView.contentInset = UIEdgeInsets(top: pageControllerContainerView.frame.height, left: 0, bottom: 0, right: 0)
+
+        pageContainerInitialHeight = pageControllerContainerView.frame.height
+
+        // update table view content height
+        tableView.layoutIfNeeded()
+
+        // adjust scroll view content height using rootContainer height anchor
+        rootContainerHeightAnchor.constant = tableView.contentSize.height + pageContainerInitialHeight
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30 // random number of cells
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+
+        let y = scrollView.contentOffset.y - tableView.contentOffset.y
+
+        pageControllerContainerTopAnchor.constant = y - pageContainerInitialHeight
+
+        if pageControllerContainerTopAnchor.constant > 0 || pageControllerContainerTopAnchor.constant == -pageContainerInitialHeight {
+            pageControllerContainerTopAnchor.constant = 0
+        }
+    }
+    
+}
+
---
+++ Medium-UITableViewHeader/README.md
@@ -0,0 +1,14 @@
+###Sticky header in UITableView
+
+复杂滚动视图的实现
+
+参考
+
+```
+https://medium.com/@shadberrow/sticky-header-in-uitableview-62621b6fc1af
+```
+![苹果效果图](https://cdn-images-1.medium.com/max/1600/1*KfL3DdSyIEQgVM775QNjQQ.gif "苹果效果图")
+
+![效果图](https://cdn-images-1.medium.com/max/1600/1*hyZtCBYHje34_ZCMTMRQjQ.gif "效果图")
+
+
